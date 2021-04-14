86f02b6eb42c399205a577ed583b809050dd8938
B1ueberry121 <2710@holbertonschool.com>
2021-04-14 23:47:11 +0000

    adding new prototypes and libraries

--- holberton.h
+++ holberton.h
@@ -3,16 +3,21 @@
 
 #include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <pwd.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
-#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <errno.h>
 
 extern char **environ;
+int __attribute((unused)) EXIT_CODE;
 
+void _free(char *line, char **args, char *path, char **drc);
+void _checkbuilts(char *line);
+char **_path(char *path);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
 int _putchar(char ch);
 int _puts(char *str);
 int _strlen(char *str);
@@ -20,7 +25,7 @@ int _strcmp(char *str1, char *str2);
 char *get_input(void);
 char **str_to_arg(char *line);
 int spw_process(char *args[]);
-char *_getenv(const char *name);
-
+char *_getenv(const char *nvs);
+void _sighandler(int sig_num);
 
 #endif
